c628264fc8795d48dec4faea46914075b6a8e1ce
syuya2036 <f20065@g.ichinoseki.ac.jp>
2024-05-12 14:20:13 +0900

    0番での操作についてテストコードを追加

--- py/board/test_board.py
+++ py/board/test_board.py
@@ -1,5 +1,5 @@
 import numpy as np
-from board import Board as bd
+from board import Board
 
 board_grid = np.array(
     [
@@ -12,6 +12,8 @@ board_grid = np.array(
     ]
 )
 
+board = Board(board_grid)
+
 cut_grid = np.array(
     [
         [0, 1, 0],
@@ -22,56 +24,121 @@ cut_grid = np.array(
 
 
 def test_op_right():
-    board = bd(board_grid)
     new = board.op_right(cut_grid, 1, 2)
-    answer = [
-        [1, 0, 1, 1, 2, 2, 1],
-        [2, 3, 1, 1, 0, 0, 2],
-        [2, 3, 0, 1, 1, 1, 1],
-        [0, 2, 3, 0, 2, 3, 1],
-        [2, 3, 2, 2, 0, 2, 2],
-        [3, 3, 1, 0, 3, 2, 3],
+    answer = Board(
+        [
+            [1, 0, 1, 1, 2, 2, 1],
+            [2, 3, 1, 1, 0, 0, 2],
+            [2, 3, 0, 1, 1, 1, 1],
+            [0, 2, 3, 0, 2, 3, 1],
+            [2, 3, 2, 2, 0, 2, 2],
+            [3, 3, 1, 0, 3, 2, 3],
         ]
-    assert np.array_equal(new, answer)
+    )
+    assert new == answer
 
 
 def test_op_left():
-    board = bd(board_grid)
     new = board.op_left(cut_grid, 1, 2)
-    answer = [
-        [1, 0, 1, 1, 2, 2, 1],
-        [2, 3, 1, 1, 0, 0, 2],
-        [3, 0, 1, 1, 1, 1, 2],
-        [3, 0, 2, 3, 1, 0, 2],
-        [2, 2, 0, 2, 2, 2, 3],
-        [3, 3, 1, 0, 3, 2, 3],
+    answer = Board(
+        [
+            [1, 0, 1, 1, 2, 2, 1],
+            [2, 3, 1, 1, 0, 0, 2],
+            [3, 0, 1, 1, 1, 1, 2],
+            [3, 0, 2, 3, 1, 0, 2],
+            [2, 2, 0, 2, 2, 2, 3],
+            [3, 3, 1, 0, 3, 2, 3],
         ]
-    assert np.array_equal(new, answer)
+    )
+    assert new == answer
 
 
 def test_op_down():
-    board = bd(board_grid)
     new = board.op_down(cut_grid, 1, 2)
-    answer = [
-        [1, 0, 2, 2, 2, 2, 1],
-        [2, 2, 3, 1, 0, 0, 2],
-        [3, 0, 1, 1, 1, 1, 1],
-        [3, 3, 1, 1, 2, 3, 1],
-        [2, 0, 0, 2, 0, 2, 2],
-        [3, 3, 1, 0, 3, 2, 3],
+    answer = Board(
+        [
+            [1, 0, 2, 2, 2, 2, 1],
+            [2, 2, 3, 1, 0, 0, 2],
+            [3, 0, 1, 1, 1, 1, 1],
+            [3, 3, 1, 1, 2, 3, 1],
+            [2, 0, 0, 2, 0, 2, 2],
+            [3, 3, 1, 0, 3, 2, 3],
         ]
-    assert np.array_equal(new, answer)
+    )
+    assert new == answer
 
 
 def test_op_up():
-    board = bd(board_grid)
     new = board.op_up(cut_grid, 1, 2)
-    answer = [
-        [1, 0, 1, 1, 2, 2, 1],
-        [2, 3, 1, 1, 0, 0, 2],
-        [3, 0, 0, 1, 1, 1, 1],
-        [3, 3, 1, 2, 2, 3, 1],
-        [2, 0, 2, 0, 0, 2, 2],
-        [3, 2, 3, 2, 3, 2, 3],
+    answer = Board(
+        [
+            [1, 0, 1, 1, 2, 2, 1],
+            [2, 3, 1, 1, 0, 0, 2],
+            [3, 0, 0, 1, 1, 1, 1],
+            [3, 3, 1, 2, 2, 3, 1],
+            [2, 0, 2, 0, 0, 2, 2],
+            [3, 2, 3, 2, 3, 2, 3],
         ]
-    assert np.array_equal(new, answer)
+    )
+    assert new == answer
+
+
+def test_one_up():
+    new = board.one_up(1, 2)
+    answer = Board(
+        [
+            [1, 0, 1, 1, 2, 2, 1],
+            [2, 3, 1, 1, 0, 0, 2],
+            [3, 0, 2, 1, 1, 1, 1],
+            [3, 2, 0, 2, 2, 3, 1],
+            [2, 3, 3, 2, 0, 2, 2],
+            [3, 0, 1, 0, 3, 2, 3],
+        ]
+    )
+    print(new.board)
+    assert new == answer
+
+
+def test_one_down():
+    new = board.one_down(0, 4)
+    answer = Board(
+        [
+            [2, 0, 1, 1, 2, 2, 1],
+            [1, 3, 1, 1, 0, 0, 2],
+            [2, 0, 2, 1, 1, 1, 1],
+            [3, 0, 0, 2, 2, 3, 1],
+            [3, 2, 3, 2, 0, 2, 2],
+            [3, 3, 1, 0, 3, 2, 3],
+        ]
+    )
+    assert new == answer
+
+
+def test_one_left():
+    new = board.one_left(1, 2)
+    answer = Board(
+        [
+            [1, 0, 1, 1, 2, 2, 1],
+            [2, 3, 1, 1, 0, 0, 2],
+            [3, 2, 1, 1, 1, 1, 0],
+            [3, 0, 0, 2, 2, 3, 1],
+            [2, 2, 3, 2, 0, 2, 2],
+            [3, 3, 1, 0, 3, 2, 3],
+        ]
+    )
+    assert new == answer
+
+
+def test_one_right():
+    new = board.one_right(5, 3)
+    answer = Board(
+        [
+            [1, 0, 1, 1, 2, 2, 1],
+            [2, 3, 1, 1, 0, 0, 2],
+            [3, 0, 2, 1, 1, 1, 1],
+            [3, 3, 0, 0, 2, 2, 1],
+            [2, 2, 3, 2, 0, 2, 2],
+            [3, 3, 1, 0, 3, 2, 3],
+        ]
+    )
+    assert new == answer
