eebf71b202a6dea905a18dfca3e4172bac66603a
syuya2036 <f20065@g.ichinoseki.ac.jp>
2024-10-03 12:27:48 +0900

    test_formal_cut_operateを実装しpassすることを確認

--- rs/tests/test_board.rs
+++ rs/tests/test_board.rs
@@ -227,3 +227,30 @@ fn test_get_formal_cut() {
         }
     }
 }
+
+#[test]
+fn test_formal_cut_operate() {
+    let mut start = Board::new(vec![
+        vec![1, 0, 1, 1, 2, 2, 1],
+        vec![2, 3, 1, 1, 0, 0, 2],
+        vec![3, 0, 2, 1, 1, 1, 1],
+        vec![3, 0, 0, 2, 2, 3, 1],
+        vec![2, 2, 3, 2, 0, 2, 2],
+        vec![3, 3, 1, 0, 3, 2, 3],
+    ]);
+
+    let cuts = Cuts::new("../data/formal_cuts.json".to_string());
+    let action = Action::new(0, 4, 1, Direction::Left);
+
+    let end = Board::new(vec![
+        vec![1, 0, 1, 1, 2, 2, 1],
+        vec![2, 3, 1, 1, 0, 0, 2],
+        vec![3, 0, 2, 1, 1, 1, 1],
+        vec![3, 0, 0, 2, 2, 3, 1],
+        vec![3, 2, 0, 2, 2, 2, 2],
+        vec![1, 0, 3, 2, 3, 3, 3],
+    ]);
+
+    start.operate(&action, &cuts);
+    assert_eq!(start, end);
+}
