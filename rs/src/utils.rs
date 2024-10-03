0fd4483557ae8dffb8b28ff9cf47e250398f2700
syuya2036 <f20065@g.ichinoseki.ac.jp>
2024-10-03 14:44:39 +0900

    utilsを作成

---
+++ rs/src/utils.rs
@@ -0,0 +1,37 @@
+use crate::board::action::{Action, Direction};
+use serde::{Deserialize, Serialize};
+
+#[derive(Serialize, Deserialize)]
+struct ActionFormat {
+    p: u16,
+    x: i32,
+    y: i32,
+    s: String,
+}
+
+#[derive(Serialize, Deserialize)]
+struct ActionsFormat {
+    actions: Vec<ActionFormat>,
+}
+
+pub fn export_actions(actions: Vec<Action>) {
+    let mut actions_format = ActionsFormat {
+        actions: Vec::new(),
+    };
+    for action in actions {
+        let action_format = ActionFormat {
+            p: action.cut_num(),
+            x: action.x(),
+            y: action.y(),
+            s: match action.direction() {
+				Direction::Up => "0".to_string(),
+				Direction::Down => "1".to_string(),
+				Direction::Right => "2".to_string(),
+				Direction::Left => "3".to_string(),
+            },
+        };
+        actions_format.actions.push(action_format);
+    }
+    let json = serde_json::to_string(&actions_format).unwrap();
+    println!("{}", json);
+}
