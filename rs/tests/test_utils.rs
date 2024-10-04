2ca92160b5fe214ed9c94b699f287a4f8c5d5052
syuya2036 <f20065@g.ichinoseki.ac.jp>
2024-10-04 14:18:30 +0900

    ボードのランダム生成を実装

--- rs/tests/test_utils.rs
+++ rs/tests/test_utils.rs
@@ -1,4 +1,5 @@
 use rs::board::action::{Action, Direction};
+use rs::board::board::Board;
 use rs::utils::export_actions;
 #[test]
 fn test_export_actions() {
@@ -12,4 +13,13 @@ fn test_export_actions() {
     let json = export_actions(actions);
     let collect = r#"{"n":4,"ops":[{"p":1,"x":1,"y":1,"s":"0"},{"p":2,"x":2,"y":2,"s":"1"},{"p":3,"x":3,"y":3,"s":"2"},{"p":4,"x":4,"y":4,"s":"3"}]}"#.to_string();
     assert_eq!(json, collect);
+}
+
+#[test]
+fn test_random_board() {
+    let board = Board::new(rs::utils::random_board(10, 10));
+    let shuffled_board = Board::new(rs::utils::shuffle_board(board.clone().board, 42));
+
+    println!("{}", board);
+    println!("{}", shuffled_board);
 }
