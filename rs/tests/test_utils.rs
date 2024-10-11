880f613e18a3ffca880fda4c10bb49865deb525f
iwbchi <hfffdde2004@gmail.com>
2024-10-11 11:38:04 +0900

    テスト削除

--- rs/tests/test_utils.rs
+++ rs/tests/test_utils.rs
@@ -48,14 +48,3 @@ fn test_get_actions() {
 //     }
 //     println!("{}", sized_actions.len());
 // }
-
-// #[test]
-// fn make_compress_action_json() {
-//     let cuts = Cuts::new("../data/formal_cuts.json".to_string());
-//     for size in vec![32, 40, 50,  64, 80, 100, 128, 256] {
-//         let actions = get_actions(size, size, &cuts);
-//         let json_str = export_actions(actions);
-//         let mut file = File::create(format!("output_{}*{}.json", size, size)).unwrap();
-//         file.write_all(json_str.as_bytes()).unwrap();
-//     }
-// }
