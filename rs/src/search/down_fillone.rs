85901d2cf65277482875e4c8fdc2438f2e15b947
syuya2036 <f20065@g.ichinoseki.ac.jp>
2024-10-15 09:11:49 +0900

    down filloneを実装

--- rs/src/search/down_fillone.rs
+++ rs/src/search/down_fillone.rs
@@ -6,35 +6,92 @@ pub fn play<'a>(
     start: &Board,
     end: &Board,
     legal_actions: &Vec<Action>,
-    count: usize,
-    log: bool,
+    cuts: &Cuts,
 ) -> Vec<Action> {
-    let mut down_only_actions = get_action_by_direction(&legal_actions, Direction::Down);
-    // leftとrightを取得して結合する
-    let mut x_only_actions = get_action_by_direction(&legal_actions, Direction::Left);
-    x_only_actions.append(&mut get_action_by_direction(
-        &legal_actions,
-        Direction::Right,
-    ));
+    let (down_only_actions, x_only_actions) =
+        get_action_by_direction(&legal_actions, Direction::Down);
 
     let mut actions = Vec::new();
+    let mut now_board = start.clone();
+    let mut already_height = start.height();
+    for i in 0..20 {
+        println!("{}", now_board);
+        println!("height: {}", already_height);
 
-    // TODO: タイムキーパーを設定する
-    for i in 0..count {
-        // 上に寄せられるだけ寄せる
-        let now_board = start.clone();
+        let mut prev_diff_len = std::u64::MAX;
+        let mut min_diff = vec![];
+        loop {
+            let (action, diff_len, diff) =
+                down_greedy_action(&now_board, &end, &down_only_actions, &cuts, already_height);
+            if prev_diff_len <= diff_len {
+                println!("break action: {:?}", action);
+                break;
+            }
+            now_board.operate(&action, &cuts);
+
+            prev_diff_len = diff_len;
+            actions.push(action);
+            min_diff = diff;
+            if diff_len == 0 {
+                break;
+            }
+        }
+
+        if !min_diff.is_empty() {
+            let action = greedy_match_x_direction_action(
+                &now_board,
+                &end,
+                &x_only_actions,
+                &cuts,
+                &min_diff,
+                already_height,
+            );
+            now_board.operate(&action, &cuts);
+            actions.push(action);
+            continue;
+        }
+
+        if already_height == 0 {
+            break;
+        }
+        now_board.op_row_up();
+        already_height -= 1;
+        actions.push(Action::new(0, -255, 22, Direction::Up));
+        if now_board == *end {
+            break;
+        }
     }
+
     actions
 }
 
-pub fn get_action_by_direction(legal_actions: &Vec<Action>, direction: Direction) -> Vec<Action> {
-    let mut down_only = Vec::new();
-    for action in legal_actions {
-        if action.direction() == direction {
-            down_only.push(action.clone());
+pub fn greedy_match_x_direction_action(
+    now_board: &Board,
+    end: &Board,
+    x_only_legal_actions: &Vec<Action>,
+    cuts: &Cuts,
+    diff: &Vec<usize>,
+    already_height: usize,
+) -> Action {
+    let mut min_distance: u64 = std::u64::MAX;
+    let mut min_action = Action::new(0, 0, 0, Direction::Down);
+
+    for action in x_only_legal_actions {
+        let mut next_board = now_board.clone();
+
+        if action.y() + cuts[action.cut_num() as u32].height() as i32 > already_height as i32 {
+            continue;
+        }
+
+        next_board.operate(action, cuts);
+        let distance = next_board.match_x_direction_score(&end, &diff, already_height);
+        if min_distance > distance {
+            min_distance = distance;
+            min_action = action.clone();
         }
     }
-    down_only
+
+    min_action
 }
 
 pub fn down_greedy_action(
@@ -42,18 +99,46 @@ pub fn down_greedy_action(
     end: &Board,
     down_only_legal_actions: &Vec<Action>,
     cuts: &Cuts,
-) -> Action {
-    let mut min_distance: u64 = 100000;
-    let mut min_action = down_only_legal_actions[0].clone();
+    already_height: usize,
+) -> (Action, u64, Vec<usize>) {
+    let mut min_distance: u64 = std::u64::MAX;
+    let mut min_action = Action::new(0, 0, 0, Direction::Down);
+    let mut min_diff = vec![std::usize::MAX];
 
     for action in down_only_legal_actions {
         let mut next_board = now_board.clone();
+        if action.y() + cuts[action.cut_num() as u32].height() as i32 > already_height as i32 {
+            continue;
+        }
         next_board.operate(action, cuts);
-        let distance = next_board
+        let (distance, diff) = next_board.top_first_distance(&end, already_height);
+        println!("action: {:?}, diff: {:?}", action, min_diff);
+
         if distance < min_distance {
             min_distance = distance;
             min_action = action.clone();
+            min_diff = diff;
         }
     }
-    min_action
+
+    (min_action, min_distance, min_diff)
+}
+
+pub fn get_action_by_direction(
+    legal_actions: &Vec<Action>,
+    direction: Direction,
+) -> (Vec<Action>, Vec<Action>) {
+    let mut down_only = Vec::new();
+    let mut x_only = Vec::new();
+    for action in legal_actions {
+        if action.y() < 1 {
+            continue;
+        }
+        if action.direction() == direction {
+            down_only.push(action.clone());
+        } else if action.direction() == Direction::Left || action.direction() == Direction::Right {
+            x_only.push(action.clone());
+        }
+    }
+    (down_only, x_only)
 }
