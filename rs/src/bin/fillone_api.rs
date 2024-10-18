4a2dc578b06aa404a0af3c9b49f31e072b27ad78
syuya2036 <f20065@g.ichinoseki.ac.jp>
2024-10-19 04:09:32 +0900

    変更に伴い修正

--- rs/src/bin/fillone_api.rs
+++ rs/src/bin/fillone_api.rs
@@ -5,7 +5,8 @@ use rs::utils::{export_post_json, string_to_board};
 async fn main() {
     let get_url = String::from("http://localhost:3000/problem");
     let post_url = String::from("http://localhost:3000/answer");
-    let data = get(get_url).await;
+    let token = "token1".to_string();
+    let data = get(get_url, token.clone()).await;
     let start = string_to_board(data.board.start);
     let end = string_to_board(data.board.goal);
 
@@ -15,6 +16,6 @@ async fn main() {
     // let actions = vec![Action::new(0, 2, 0, Direction::Up)];
     let json = export_post_json(&actions);
     println!("{}", json);
-    let res = post(post_url, json);
+    let res = post(post_url, json, token);
     println!("{:?}", res.await);
 }
--- rs/src/bin/get_data_api.rs
+++ rs/src/bin/get_data_api.rs
@@ -4,6 +4,7 @@ use rs::client::get;
 async fn main() {
     let test_num = 0;
     let url = format!("http://127.0.0.1:3000/{}/", test_num);
-    let data = get(url).await;
+    let token = "token1".to_string();
+    let data = get(url, token).await;
     println!("{:?}", data.general.patterns[0]);
 }
