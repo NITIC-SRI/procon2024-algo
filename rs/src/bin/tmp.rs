f7f96e0780ec0f53806c4b53ce7191ca989beabb
syuya2036 <f20065@g.ichinoseki.ac.jp>
2024-10-19 02:32:42 +0900

    本番用のテスト

--- rs/src/bin/tmp.rs
+++ rs/src/bin/tmp.rs
@@ -1,9 +1,7 @@
-use rs::board::cut::Cuts;
-use rs::search::down_fillone::play;
-use rs::{board::board::Board, utils};
+use rs::board::board::Board;
+use rs::utils;
 
 fn main() {
-    let size = 32;
     let start: Board<u8> = Board::new(vec![
         vec![
             0, 1, 0, 2, 2, 0, 2, 2, 2, 3, 3, 2, 1, 0, 3, 1, 0, 0, 1, 0, 1, 2, 0, 1, 2, 3, 1, 2, 0,
@@ -134,7 +132,7 @@ fn main() {
             0, 2, 2,
         ],
     ]);
-    let end = Board::new(vec![
+    let end: Board<u8> = Board::new(vec![
         vec![
             2, 3, 1, 2, 0, 0, 2, 1, 2, 0, 1, 2, 0, 1, 3, 1, 1, 0, 2, 0, 1, 1, 2, 0, 0, 2, 0, 3, 3,
             2, 3, 0,
@@ -264,29 +262,13 @@ fn main() {
             3, 2, 2,
         ],
     ]);
-    // let start = Board::new(utils::random_board(size, size));
-    // let end = Board::new(utils::shuffle_board(start.board().clone(), 42));
 
-
-    let path = "../data/formal_cuts.json".to_string();
-    let cuts = Cuts::new(path);
-
-    // let legal_actions = utils::get_actions(size as usize, size as usize, &cuts);
-    let legal_actions = utils::read_actions_by_size(size as usize, size as usize);
-
-    let actions = play(&start, &end, &legal_actions, &cuts);
-    let json = utils::export_visualyzer_json(&start, &end, actions.clone());
-
-    println!("{}", json);
-    println!("len: {}", actions.len());
-
-    let mut now = start.clone();
-    for action in actions {
-        now.operate(&action, &cuts);
-    }
-
-    println!("{}", now);
-    println!("---");
-    println!("{}", end);
-    println!("score: {}", now.absolute_distance(&end));
+	for h in 0..start.height() {
+		print!("\"");
+		for w in 0..start.width() {
+			print!("{}", end.board()[h][w]);
+		}
+		// ダブルクウォーテーションで囲む
+		println!("\",");
+	}
 }
--- rs/src/main.rs
+++ rs/src/main.rs
@@ -1,3 +1,29 @@
-fn main() {
-    println!("Hello, world!");
+use rs::client::{get, post};
+use rs::utils::{export_post_json, string_to_board};
+use rs::board::cut::Cuts;
+use rs::utils;
+use rs::search::down_fillone::play;
+
+#[tokio::main]
+async fn main() {
+    let get_url = String::from("http://localhost:3000/problem");
+    let post_url = String::from("http://localhost:3000/answer");
+    let data = get(get_url).await;
+    let start = string_to_board(data.board.start);
+    let end = string_to_board(data.board.goal);
+    let size_h = data.board.height;
+    let size_w = data.board.width;
+
+    let path = "../data/formal_cuts.json".to_string();
+    let cuts = Cuts::new(path);
+
+    let legal_actions = utils::read_actions_by_size(size_w as usize, size_h as usize);
+
+    let actions = play(&start, &end, &legal_actions, &cuts);
+
+    // let actions = vec![Action::new(0, 2, 0, Direction::Up)];
+    let json = export_post_json(&actions);
+    println!("{}", json);
+    let res = post(post_url, json);
+    println!("{:?}", res.await);
 }
